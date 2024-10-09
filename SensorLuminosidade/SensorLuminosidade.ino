0d50696c9c51ff166e0f4b96eef59163a933b00c
matheussteinbach <matheussteimbach4@gmail.com>
2024-10-09 20:09:59 -0300

    feat: print do valor de luz e do potenciometro

--- SensorLuminosidade/SensorLuminosidade.ino
+++ SensorLuminosidade/SensorLuminosidade.ino
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 
-
-// Valor Permanente do Pin
 const int sensorPin = 15;
 const int ledPin = 4;
 const int potenPin = 34;
@@ -9,24 +7,29 @@ const int potenPin = 34;
 void setup() {
   Serial.begin(115200);
   while(!Serial);
-  Serial.println("INICIEI!");
   
   // Inicialização
   pinMode(ledPin, OUTPUT);
   pinMode(sensorPin, INPUT);
   pinMode(potenPin, INPUT);
+  Serial.println("INICIEI!");
 }
 
 void loop() {
-  // Ler valor do estado do sensor de iluminação
+  // Ler valor do sensor de iluminação
   int valLuz = analogRead(sensorPin);
   int valPoten = analogRead(potenPin);
 
-  // HIGH = Apertado
+  // Valor baixo = Muita luz, Valor alto = Pouca luz
+  Serial.print("Luz:");
+  Serial.println(valLuz);
+  Serial.print("Potenciometro:");
   Serial.println(valPoten);
   if (valLuz > valPoten) {
     digitalWrite(ledPin, HIGH);
   } else {
     digitalWrite(ledPin, LOW);
   }
+
+  delay(100);
 }
